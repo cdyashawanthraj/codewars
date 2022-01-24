2bc555071d8b93d5d2b90a3f33ff5a19a91d7320
cdyashawanthraj <83272234+cdyashawanthraj@users.noreply.github.com>
2022-01-24 16:16:53 +0530

    Hello SQL World!.sql

---
+++ sql/Hello SQL World!.sql
@@ -0,0 +1,10 @@
+/*
+Hello SQL!
+
+Return a table with a single column named Greeting with the phrase 'hello world!'
+*/
+
+--sql
+select 'hello world!' as "Greeting"
+
+--we need to use double quotes for the column name in the kata training to get column as we write.
